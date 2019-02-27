ffe227c35ed33b3ebabae63eb2c3f0f0e8ddaa43
eric-yoo <ajoije729@gmail.com>
2019-02-27 21:18:12 +0900

    fix 1~5

---
+++ notebook/module.py
@@ -0,0 +1,10 @@
+names=['가','나','다']
+
+def read_input(count):
+    result=[]
+    for i in range(count): #[0,1,2,3,..., count-1]
+        a=input()
+        result.append(a)
+    print(result)
+
+    
